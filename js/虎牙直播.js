fe2674b6fa60e8b7cdc5858cc00b5ed8c61475d2
gao410940747 <410940747@qq.com>
2023-12-04 16:31:22 +0800

    守望先锋虎牙

--- js/虎牙直播.js
+++ js/虎牙直播.js
@@ -4,12 +4,13 @@ var rule = {
     // 默认守望先锋归来"2174"
     homeUrl:'/cache.php?m=LiveList&do=getLiveListByPage&gameId=&tagAll=0&page=1',//用于"分类获取"和"推荐获取"
     url:'/cache.php?m=LiveList&do=getLiveListByPage&gameId=fyfilter&tagAll=0&page=fypage',
-    class_name:'娱乐&网游&单机&手游',
-    class_url:'8&1&2&3',
+    class_name:'守望先锋&娱乐&网游&单机&手游',
+    class_url:'2174&8&1&2&3',
     detailUrl:'https://m.huya.com/fyid',//二级详情拼接链接(json格式用)
     filterable: 1,
     filter_url: '{{fl.cateId}}',
     filter_def:{
+        2174:{cateId:'2174'},
         8:{cateId:'2135'},
         // 默认守望先锋归来"2174"
         1:{cateId:'2174'},
