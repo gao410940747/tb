e5d3ab21f7c0ca7b8ed0e7d4fe8929be73f635d5
gao410940747 <43198424+gao410940747@users.noreply.github.com>
2023-12-06 23:57:28 +0800

    更新 荐片.js

--- diy/荐片.js
+++ diy/荐片.js
@@ -7,7 +7,7 @@ var rule = {
     title:'荐片',
     host:'http://api2.rinhome.com',
     // homeUrl:'https://yjpapipxblwdohpakljwg.hxhzs.com/api/tag/hand?code=unknown601193cf375db73d&channel=wandoujia',//网站的首页链接,用于分类获取和推荐获取
-//    homeUrl:'/api/tag/hand?code=unknown601193cf375db73d&channel=wandoujia',//网站的首页链接,用于分类获取和推荐获取
+    homeUrl:'/api/tag/hand?code=unknown601193cf375db73d&channel=wandoujia',//网站的首页链接,用于分类获取和推荐获取
     // url:'/api/crumb/list?area=0&category_id=fyclass&page=fypage&type=0&limit=24&fyfilter',
     url:'/api/crumb/list?area=0&page=fypage&type=0&limit=24&fyfilter',
     class_name:'全部&综艺&电影&电视剧&动漫',     // 筛选 /api/term/ad_fenlei?limit=10&page=1
