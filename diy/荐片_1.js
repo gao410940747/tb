cb4969950d491ed7bbb0acad658b9bb0e8819382
gao410940747 <43198424+gao410940747@users.noreply.github.com>
2024-01-01 02:26:03 +0800

    更新 荐片_1.js

--- diy/荐片_1.js
+++ diy/荐片_1.js
@@ -6,7 +6,7 @@
 var rule = {
     title:'荐片',
     host:'http://api2.rinhome.com',
-    homeUrl:'/api/tag/hand?code=unknown601193cf375db73d&channel=wandoujia',//网站的首页链接,用于分类获取和推荐获取
+//    homeUrl:'/api/tag/hand?code=unknown601193cf375db73d&channel=wandoujia',//网站的首页链接,用于分类获取和推荐获取
     url:'/api/crumb/list?area=0&page=fypage&type=0&limit=24&fyfilter',
     class_name:'综艺&电影&电视剧&动漫&全部',     // 筛选 /api/term/ad_fenlei?limit=10&page=1
     class_url:'4&1&2&3&0',
@@ -48,7 +48,7 @@ var rule = {
     // double:true,
     图片来源:'@Referer=www.jianpianapp.com@User-Agent=jianpian-version353',
     // 推荐:'json:data;room;*;cover;*;*',
-    推荐:'json:.video;*;*;*;*',
+//    推荐:'json:.video;*;*;*;*',
     一级:'json:data;title;path;playlist.title;id',
     二级:{
         // title:'data.title',
