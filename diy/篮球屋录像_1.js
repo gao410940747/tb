a24cd5129b5401fcbe62fcc11360366aacf5737a
gao410940747 <43198424+gao410940747@users.noreply.github.com>
2024-01-07 07:10:55 +0800

    更新 篮球屋录像_1.js

--- diy/篮球屋录像_1.js
+++ diy/篮球屋录像_1.js
@@ -7,7 +7,7 @@ var rule = {
     searchable:0,
     quickSearch:0,
     class_name:'NBA录像&NBA集锦&NBA十佳球&CBA录像&CBA集锦&其他篮球录像&快船比赛',
-    class_url:'nbalx&nbajijin&nbatop10&cbalx&cbajijin&lanqiulx&qiudui/kuaichuan',
+    class_url:'nbalx&nbajijin&nbatop10&cbalx&cbajijin&lanqiulx&qiudui/kuaichuan/',
     headers:{
         'User-Agent':'PC_UA'
     },
