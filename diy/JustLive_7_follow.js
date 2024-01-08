b21e7da9ceafeb5068a4dd243f569deef5ab2572
gao410940747 <43198424+gao410940747@users.noreply.github.com>
2024-01-08 23:22:12 +0800

    (需使用EXO播放器)

--- diy/JustLive_7_follow.js
+++ diy/JustLive_7_follow.js
@@ -390,9 +390,9 @@ var rule = {
             let playList = [];
 
             var playUrls = info[6].split("；")
-            playFrom.append('官方线路(需使用EXO播放器)');
+            playFrom.append('官方线路');
             playList.append(playUrls.map(function(it) {
-                return it.split("，")[0].replace("快手", "") + "$" + it.split("，")[1]
+                return it.split("，")[0].replace("快手", "") + '(需使用EXO播放器)' + "$" + it.split("，")[1]
             }).join("#"));
 
             // 最后封装所有线路
@@ -451,10 +451,14 @@ var rule = {
 
             // JustLive获取源
             Object.keys(playurl).forEach(function(key) {
-            if (jo.platForm == 'huya') {playFrom.append('官方' + key + '(需使用EXO播放器)');}else{playFrom.append('官方' + key);}
-                
-                playList.append(playurl[key].map(function(it) {
-                    return it.qualityName + "$" + it.playUrl
+playFrom.append('官方' + key);
+playList.append(playurl[key].map(function(it) {
+
+            if (jo.platForm == 'huya') {
+return it.qualityName + '(需使用EXO播放器)' + "$" + it.playUrl
+} else {
+return it.qualityName + "$" + it.playUrl
+}
                 }).join("#"))
             });
 
