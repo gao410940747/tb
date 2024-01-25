88285cf2d7c055ea3d1d55baa242af912ccf1175
gao410940747 <410940747@qq.com>
2024-01-25 14:16:53 +0800

    隐藏推荐页，隐藏88看球分类

--- diy/88看球_4.js
+++ diy/88看球_4.js
@@ -8,63 +8,61 @@ var rule = {
     searchUrl:'',
     searchable:0,
     quickSearch:0,
-    class_name: 'JRS赛事直播',
-    class_url: 'jrkan',
-//    class_name: 'JRS赛事直播&快船赛程',
-//    class_url: 'jrkan&clippers',
-    class_parse:'.nav-pills li;a&&Text;a&&href;/match/(\\d+)/live',
+    class_name: 'JRS赛事直播&88看球',
+    class_url: 'jrkan&88kanqiu',
+//    class_parse:'.nav-pills li;a&&Text;a&&href;/match/(\\d+)/live',
     headers:{
         'User-Agent':'PC_UA'
     },
     timeout:10000,
     play_parse:true,
     lazy:'',
-    limit:6,
+    limit:8,
     double:false,
     // 推荐:'*',
-    推荐:`js:
-        var items=[];
-        pdfh=jsp.pdfh;
-        pdfa=jsp.pdfa;
-        pd=jsp.pd;
-        var html=request(input);
-        var tabs=pdfa(html,'.list-group&&.list-group-item');
-
-        // 定义日期
-        var date = '';
-
-        tabs.forEach(function(it){
-            // 通过" "进行截取
-            let split = pdfh(it, '.d-none&&Text').split(" ");
-
-            if(/undefined/.test(split[2])){
-                date = pdfh(it, 'li&&Text').split('-')[1] + '-' + pdfh(it, 'li&&Text').split('-')[2] + ' ';
-            } else {
-                // 一级标题
-                let title1 = split[2] + '🆚' + split[4];
-                // 一级描述
-                let desc1 = date + split[0] + ' ' +  split[1] + ' ' + pdfh(it, '.btn&&Text');
-                // 一级图片URL
-                let picUrl1 = pd(it,'.col-xs-1 img&&src');
-                // 一级URL
-                let url1 = pd(it, '.btn&&href') + '|' + '88kanqiu';
-
-                items.push({
-                    desc:desc1,
-                    title:title1,
-                    pic_url:picUrl1,
-                    url:url1
-                })
-            }
-        });
-        setResult(items);
-    `,
+//    推荐:`js:
+//        var items=[];
+//        pdfh=jsp.pdfh;
+//        pdfa=jsp.pdfa;
+//        pd=jsp.pd;
+//        var html=request(input);
+//        var tabs=pdfa(html,'.list-group&&.list-group-item');
+//
+//        // 定义日期
+//        var date = '';
+//
+//        tabs.forEach(function(it){
+//            // 通过" "进行截取
+//            let split = pdfh(it, '.d-none&&Text').split(" ");
+//
+//            if(/undefined/.test(split[2])){
+//                date = pdfh(it, 'li&&Text').split('-')[1] + '-' + pdfh(it, 'li&&Text').split('-')[2] + ' ';
+//            } else {
+//                // 一级标题
+//                let title1 = split[2] + '🆚' + split[4];
+//                // 一级描述
+//                let desc1 = date + split[0] + ' ' +  split[1] + ' ' + pdfh(it, '.btn&&Text');
+//                // 一级图片URL
+//                let picUrl1 = pd(it,'.col-xs-1 img&&src');
+//                // 一级URL
+//                let url1 = pd(it, '.btn&&href') + '|' + '88kanqiu';
+//
+//                items.push({
+//                    desc:desc1,
+//                    title:title1,
+//                    pic_url:picUrl1,
+//                    url:url1
+//                })
+//            }
+//        });
+//        setResult(items);
+//    `,
     // 一级:'.list-group .group-game-item;.d-none&&Text;.team-logo&&src;.btn&&Text;.btn&&href',
     一级:`js:
-        var items=[];
-        pdfh=jsp.pdfh;
-        pdfa=jsp.pdfa;
-        pd=jsp.pd;
+        var items = [];
+        pdfh = jsp.pdfh;
+        pdfa = jsp.pdfa;
+        pd = jsp.pd;
         var html;
         if(MY_PAGE===1){
             if(MY_CATE==='jrkan'){
@@ -83,6 +81,38 @@ var rule = {
                         pic_url:img,
                         url: url + '|' + 'jrkan'
                     });
+                });
+            }
+            else if(MY_CATE==='88kanqiu'){
+                html = request(HOST);
+                var tabs = pdfa(html,'.list-group&&.list-group-item');
+
+                // 定义日期
+                var date = '';
+
+                tabs.forEach(function(it){
+                    // 通过" "进行截取
+                    let split = pdfh(it, '.d-none&&Text').split(" ");
+
+                    if(/undefined/.test(split[2])){
+                        date = pdfh(it, 'li&&Text').split('-')[1] + '-' + pdfh(it, 'li&&Text').split('-')[2] + ' ';
+                    } else {
+                        // 一级标题
+                        let title1 = split[2] + '🆚' + split[4];
+                        // 一级描述
+                        let desc1 = date + split[0] + ' ' + pdfh(it, '.btn&&Text');
+                        // 一级图片URL
+                        let picUrl1 = (MY_CATE==='clippers' && split[2]==='快船')?pd(it,'.team-logo&&src'):pd(it,'.col-xs-1 img&&src');
+                        // 一级URL
+                        let url1 = pd(it, '.btn&&href');
+
+                        items.push({
+                            desc:desc1,
+                            title:title1,
+                            pic_url:picUrl1,
+                            url:url1 + '|' + '88kanqiu'
+                        })
+                    }
                 });
             }
             else {
@@ -192,10 +222,10 @@ var rule = {
                 type_name: pdfh(new_html,'.customer-navbar-nav&&li&&Text'),
                 vod_content: pdfh(new_html,'.col-md-4:eq(1)&&Text').replaceAll(' ', '_'),
             };
-    
+
             // 播放列表拼接
             var playListStr = '';
-    
+
             var playUrls = JSON.parse(request(url.replace(HOST+'88kanqiu%7C', '')+'-url')).links;
             playUrls.map(function(it) {
                 var name = it.name;
@@ -205,7 +235,7 @@ var rule = {
                     playListStr = playListStr + '咪咕专线'+'$'+url+'#';
                 }
             });
-    
+
             playUrls.map(function(it) {
                 var name = it.name;
                 var url = it.url;
@@ -217,16 +247,16 @@ var rule = {
                     playListStr = playListStr + '爱奇艺专线'+'$'+url+'#';
                 }
             });
-    
+
             playUrls.map(function(it) {
                 playListStr = playListStr + it.name+ '$' + it.url + '#';
             });
-    
+
             let playFrom = [];
             let playList = [];
             playFrom.append('88看球');
             playList.append(playListStr);
-    
+
             // 最后封装所有线路
             let vod_play_from = playFrom.join('$$$');
             let vod_play_url = playList.join('$$$');
