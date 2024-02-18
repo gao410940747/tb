a89222671ae50ca54ad9bc054bb865e642d92747
gao410940747 <410940747@qq.com>
2024-02-18 16:58:06 +0800

    优化荐片二级信息。

--- diy/荐片_1.js
+++ diy/荐片_1.js
@@ -8,8 +8,8 @@ var rule = {
     host:'http://api2.rinhome.com',
     homeUrl:'/api/tag/hand?code=unknown601193cf375db73d&channel=wandoujia',//网站的首页链接,用于分类获取和推荐获取
     url:'/api/crumb/list?area=0&page=fypage&type=0&limit=24&channel=wandoujia&code=vmosseri39898b78d878c8ca&fyfilter',
-    class_name:'综艺&电影&电视剧&动漫&全部',     // 筛选 /api/term/ad_fenlei?limit=10&page=1
-    class_url:'4&1&2&3&0',
+    class_name:'全部&电影&电视剧&动漫&综艺',     // 筛选 /api/term/ad_fenlei?limit=10&page=1
+    class_url:'0&1&2&3&4',
     detailUrl:'/api/node/detail?channel=wandoujia&token=&id=fyid',//二级详情拼接链接(json格式用)
     searchUrl:'/api/video/search?key=**&page=fypage',
     searchable:1,
@@ -34,7 +34,7 @@ var rule = {
         'User-Agent':'jianpian-android/364',
         'JPAUTH':'dptq18koK6hm6mX52h1KPbUPXQ3LTZPF54qQ5n7Ff7Ta'
     },
-    timeout:5000,
+    timeout:10000,
     limit:8,
     play_parse:true,
     play_json:[{
@@ -47,46 +47,80 @@ var rule = {
     lazy:'',
     // double:true,
     图片来源:'@Referer=www.jianpianapp.com@User-Agent=jianpian-version353',
-    // 推荐:'json:data;room;*;cover;*;*',
-//    推荐:'json:.video;*;*;*;*',
+    推荐:'json:data[0].video;title;path;playlist.title;id',
     一级:'json:data;title;path;playlist.title;id',
-    二级:{
-        // title:'data.title',
-        title:'data.title;data.types[0].name',
-        // desc:';data.description;data.id',
-        desc:'data.score;data.year.title;data.area.title;data.actors[0].name;data.directors[0].name',
-        img:'data.thumbnail',
-        // content:'data.thumbnail',
-        content:'data.description',
-        is_json:1,
-        tabs:`js:
-            TABS = [];
-            if (html.data.have_ftp_ur == 1) {
-                TABS.push("边下边播超清版")
-            }
-            if (html.data.have_m3u8_ur == 1) {
-                TABS.push("在线点播普清版")
-            }
-        `,
-        lists:`js:
-            log(TABS);
-            LISTS = [];
-            TABS.forEach(function(tab) {
-                if (/边下边播/.test(tab)) {
-                    let ftp = html.data.new_ftp_list;
-                    let d = ftp.map(function(it) {
-                        return it.title + "$" + (/m3u8/.test(it.url) ? play_url + it.url : "tvbox-xg:" + it.url)
-                    });
-                    LISTS.push(d)
-                } else if (/在线点播/.test(tab)) {
-                    let m3u = html.data.new_m3u8_list;
-                    let d = m3u.map(function(it) {
-                        return it.title + "$" + (/m3u8/.test(it.url) ? play_url + it.url : "tvbox-xg:" + it.url)
-                    });
-                    LISTS.push(d)
-                }
-            });
-        `,
-    },
+    二级: `js:
+        var link = HOST + '/api/node/detail?channel=wandoujia&token=&id=' + input;
+        var data = JSON.parse(request(link)).data;
+        VOD = {
+            vod_id: data.id,
+            vod_name: data.title + ' ' + data.score + '分',
+            vod_pic: data.tvimg,
+            vod_remarks: data.others_name.map(function(it) {return it.value}).join(","),
+            type_name: data.types.map(function(it) {return it.name}).join(","),
+            vod_year: data.year.title,
+            vod_area: data.area.title,
+            vod_director: data.directors.map(function(it) {return it.name}).join(","),
+            vod_actor: data.actors.map(function(it) {return it.name}).join(","),
+            vod_content: data.description,
+        };
+        let playFrom = [];
+        let playList = [];
+        if (data.have_ftp_ur == 1) {
+            playFrom.append('边下边播超清版');
+            playList.append(data.new_ftp_list.map(function(it) {
+                return it.title + "$" + "tvbox-xg:" + it.url
+            }).join("#"))
+        }
+        if (data.have_m3u8_ur == 1) {
+            playFrom.append('在线点播普清版');
+            playList.append(data.new_m3u8_list.map(function(it) {
+                return it.title + "$" + "tvbox-xg:" + it.url
+            }).join("#"))
+        }
+        // 最后封装所有线路
+        let vod_play_from = playFrom.join('$$$');
+        let vod_play_url = playList.join('$$$');
+        VOD['vod_play_from'] = vod_play_from;
+        VOD['vod_play_url'] = vod_play_url;
+    `,
+//    二级:{
+//        // title:'data.title',
+//        title:'data.title;data.types[0].name',
+//        // desc:';data.description;data.id',
+//        desc:'data.score;data.year.title;data.area.title;data.actors[0].name;data.directors[0].name',
+//        img:'data.thumbnail',
+//        // content:'data.thumbnail',
+//        content:'data.description',
+//        is_json:1,
+//        tabs:`js:
+//            TABS = [];
+//            if (html.data.have_ftp_ur == 1) {
+//                TABS.push("边下边播超清版")
+//            }
+//            if (html.data.have_m3u8_ur == 1) {
+//                TABS.push("在线点播普清版")
+//            }
+//        `,
+//        lists:`js:
+//            log(TABS);
+//            LISTS = [];
+//            TABS.forEach(function(tab) {
+//                if (/边下边播/.test(tab)) {
+//                    let ftp = html.data.new_ftp_list;
+//                    let d = ftp.map(function(it) {
+//                        return it.title + "$" + (/m3u8/.test(it.url) ? play_url + it.url : "tvbox-xg:" + it.url)
+//                    });
+//                    LISTS.push(d)
+//                } else if (/在线点播/.test(tab)) {
+//                    let m3u = html.data.new_m3u8_list;
+//                    let d = m3u.map(function(it) {
+//                        return it.title + "$" + (/m3u8/.test(it.url) ? play_url + it.url : "tvbox-xg:" + it.url)
+//                    });
+//                    LISTS.push(d)
+//                }
+//            });
+//        `,
+//    },
     搜索:'json:data;*;thumbnail;mask;*',
 }
